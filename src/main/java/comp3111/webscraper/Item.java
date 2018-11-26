26357dcd6395a301e148d8f42a75a192d4a7a14a
Patsixd <43234487+Patsixd@users.noreply.github.com>
2018-11-26 21:39:37 +0800

    Add basic feature 3 :))

--- src/main/java/comp3111/webscraper/Item.java
+++ src/main/java/comp3111/webscraper/Item.java
@@ -8,7 +8,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Hyperlink;
 
-
+/**
+ * Item class for creating and regulating objects for storing searched data.
+ *
+ */
 public class Item {
 	private String title; 
 	private double price;
@@ -16,49 +19,116 @@ public class Item {
 	private Date postedDate;
 	private Hyperlink hyperlink;
 	
+	public void nextPageItem(String url) {
+		this.setTitle("Next Page");
+		this.setPrice(0.0);
+		this.setUrl(url);
+		this.setPostedDate("Jan 01, 1990");
+		this.setHyperlink(url);
+	}
+	public Boolean testNextPage() {
+		if(this.getTitle().equals("Next Page") && 
+				this.getPrice() == 0.0 &&
+				this.getPostedDate().equals("Jan 01, 1990"))
+			return true;
+		else
+			return false;
+	}
+	/**
+     * Getter: return the title of Item object.
+     * 
+     * @return String - Title of Item object
+     */
 	public String getTitle() {
 		return title;
 	}
+	
+	/**
+     * Setter: set the title data member of Item object.
+     * 
+     * @param title The title of Item object searched
+     */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	/**
+     * Getter: return the price of Item object.
+     * 
+     * @return Double - Price of Item object
+     */
 	public double getPrice() {
 		return price;
 	}
+	
+	/**
+     * Setter: set the price data member of Item object.
+     * 
+     * @param price The price of Item object searched
+     */
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	/**
+     * Getter: return the URL of Item object.
+     * 
+     * @return String - URL of Item object
+     */
 	public String getUrl() {
 		return url;
 	}
-	public void setUrl(String url, String url2) {
+	
+	/**
+     * Setter: set the URL data member of Item object.
+     * 
+     * @param url The URL of Item object searched
+     */
+	public void setUrl(String url) {
 		this.url = url;
+	}
+	
+	/**
+     * Getter: return the URL in Hyperlink form of Item object.
+     * 
+     * @return Hyperlink - URL of Item object
+     */
+	public Hyperlink getHyperlink() {
+		return this.hyperlink;
+	}
+	
+	/**
+     * Setter: set the Hyperlink data member of Item object.
+     * 
+     * @param url The URL of Item object searched
+     */
+	public void setHyperlink(String url) {
 		this.hyperlink = new Hyperlink();
-		this.hyperlink.setText(url2);
+		this.hyperlink.setText(url);
 		this.hyperlink.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				try {
-				    Desktop.getDesktop().browse(new URL(url2).toURI());
+				    Desktop.getDesktop().browse(new URL(url).toURI());
 				} catch (Exception be) {}
 			}
 		});
 	}
-	public Hyperlink getHyperlink() {
-		return this.hyperlink;
+	
+	/**
+     * Getter: return the posted date of Item object. Mainly for internal comparison use.
+     * 
+     * @return Date - Posted date of Item object
+     */
+	public Date getDate() { //this is for getting the posted date in Date type (for Summary Tab, comparing dates of products)
+		return this.postedDate;
 	}
-  
-	public Date getDate() {
-		return postedDate;
-	}
-  
-	public void setDate(Date inDate) {
-    this.postedDate = inDate;
-  }
-  
-	public void setPostedDate(Date inDate) {
-		this.postedDate = inDate;
-	}
+	
+	/**
+     * Getter: return the posted date of Item object. Mainly for GUI output.
+     * 
+     * @return String - Posted date of Item object
+     */
 	public String getPostedDate() {
 		String temp;
 		temp = this.postedDate.toString().substring(4, 7); //get the month
@@ -69,12 +139,20 @@ public class Item {
 		return temp;
 	}
 	
-	public void setUrl(String url) {//this is for after loading a search record
-		System.out.println(url);
-		for(int i=0; i<url.length()-8; i++) {
-			if (url.substring(i, i+8).equals("https://")) this.setUrl(url.substring(0, i), url.substring(i, url.length()));
-		}
+	/**
+     * Setter: set the posted date data member of Item object. Used with results scrapped online.
+     * 
+     * @param inDate The posted date (of Date type variable) of Item object searched
+     */
+	public void setPostedDate(Date inDate) { //this is called by WebScrapper
+		this.postedDate = inDate;
 	}
+
+	/**
+     * Setter: set the posted date data member of Item object. Used with results loaded locally.
+     * 
+     * @param inDate The posted date (of String type variable) of Item object
+     */
 	public void setPostedDate(String inDate) {//this is for after loading a search record
 		SimpleDateFormat formatter = new SimpleDateFormat("MMM dd, yyyy");
 		try{
@@ -82,3 +160,4 @@ public class Item {
 		}catch (Exception e) {}
 	}
 }
+
